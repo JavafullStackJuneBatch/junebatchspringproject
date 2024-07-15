437975b6de020f0de6feae3c1a83d9b71d979bc8
saritha <aitham.saritha@gmail.com>
2024-07-15 21:24:47 +0530

    added

---
+++ src/main/java/com/example/junebatchspringproject/JunebatchspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.junebatchspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunebatchspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunebatchspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/junebatchspringproject/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.junebatchspringproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hellow World Saritha";
+    }
+
+
+    @GetMapping("/helloSaritha")
+    public String sayHello1(){
+        return "Hellow World Saritha ---------=======";
+    }
+
+
+}
