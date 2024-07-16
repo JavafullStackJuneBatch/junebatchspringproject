b0dd025c6c9fbd92e1127478ba6cda9068afc330
akshathamb <rn.akshatha@gmail.com>
2024-07-16 16:34:07 -0400

    added sout

--- src/main/java/com/example/junebatchspringproject/controller/HelloController.java
+++ src/main/java/com/example/junebatchspringproject/controller/HelloController.java
@@ -30,6 +30,7 @@ public class HelloController {
         System.out.println("Hello Akshatha");
         System.out.println("june batch");
         System.out.println("JavaFullStack");
+        System.out.println("SpringBootProject");
         System.out.println("Hello Everyone");
 
         System.out.println("New Branch");
