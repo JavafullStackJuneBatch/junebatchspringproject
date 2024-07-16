7202f35d84449077eae173c583c04f5f54c8bc80
Neha <neha.mahant05@gmail.com>
2024-07-16 09:59:35 -0700

    New

--- src/main/java/com/example/junebatchspringproject/controller/HelloController.java
+++ src/main/java/com/example/junebatchspringproject/controller/HelloController.java
@@ -15,6 +15,7 @@ public class HelloController {
 
         System.out.println("Hello bhavana");
         System.out.println("Hello Everyone");
+        System.out.println("New Branch");
         return "Hellow World Saritha";
     }
 
