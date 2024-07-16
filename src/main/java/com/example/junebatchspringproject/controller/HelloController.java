03d2b1554993d9896bef3b751119873ae91fb2ea
akshathamb <rn.akshatha@gmail.com>
2024-07-16 16:17:16 -0400

    added sout

--- src/main/java/com/example/junebatchspringproject/controller/HelloController.java
+++ src/main/java/com/example/junebatchspringproject/controller/HelloController.java
@@ -11,24 +11,31 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         System.out.println("My first endpoint");
+
         System.out.println("Hello Suryasree");
        System.out.println("Hello swathi");
         System.out.println("Hello parvathy");
         System.out.println("Hello bhavana");
         System.out.println("Hello Worldg");
         System.out.println("Hello Akshatha");
+        System.out.println("june batch");
         System.out.println("Hello Everyone");
         System.out.println("Surachita");
         System.out.println("Hello World");
 
 
+=======
+        System.out.println("Hello ambika");
+>>>>>>> 61a509198006de23218b25afef0eb31d44297881
         return "Hellow World Saritha";
     }
 
 
     @GetMapping("/helloSaritha")
     public String sayHello1(){
+        System.out.println("creating  branch");
         return "Hellow World Saritha ---------=======";
+
     }
 
 
