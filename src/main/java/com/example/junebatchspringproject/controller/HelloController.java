be28653f462adfe402e6eb7ab67888e3a0ca8e20
Sagar Ghubade <vanitah8@gmail.com>
2024-07-21 19:47:40 -0400

    first code check in

--- src/main/java/com/example/junebatchspringproject/controller/HelloController.java
+++ src/main/java/com/example/junebatchspringproject/controller/HelloController.java
@@ -6,57 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
-
     @GetMapping("/hello")
-    public String sayHello(){
-        System.out.println("My first endpoint");
-        System.out.println("Hello Kavya");
-        System.out.println("Chk");
-        System.out.println("Hello ambika");
-
-        System.out.println("Hello Suryasree");
-
-        System.out.println("New github");
-       System.out.println("Hello swathi");
-       System.out.println("Hello Siri");
-        System.out.println("Hello parvathy");
-
-        System.out.println("Hello bhavana");
-        System.out.println("Hello Worldg");
-        System.out.println("Hello Akshatha");
-        System.out.println("june batch");
-        System.out.println("JavaFullStack");
-        System.out.println("SpringBootProject");
-        System.out.println("Github");
-        System.out.println("june batch");
-        System.out.println("JavaFullStack");
-        System.out.println("SpringBootProject");
-        System.out.println("SpringFramework");
-        System.out.println("SpringFramework24");
-
-        System.out.println("Hello Everyone");
-
-        System.out.println("New Branch");
-
-        System.out.println("Surachita");
-        System.out.println("Hello World");
-        System.out.println("Branch added");
-        System.out.println("Hi Ambika");
-
-
-
-
-        return "Hellow World Saritha";
-    }
-
-
-    @GetMapping("/helloSaritha")
-    public String sayHello1(){
-        System.out.println("creating branch");
-        System.out.println("Bhavana1836 Branch");
-        return "Hello World Saritha";
-
+    public String SayHello() {
+        return "Hello Vanita";
     }
 
 
